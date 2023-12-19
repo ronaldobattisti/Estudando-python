bc738b7e2b0ed04b67cdcfc30bc48c34023de9a7
SAZI | Ronaldo Battisti <rbattisti@sazi.com.br>
2023-12-19 16:59:16 -0300

    Adição do soma pares

---
+++ Soma_pares.py
@@ -0,0 +1,14 @@
+print("Escreva os número para o array, e para finalizar pressione enter:\n")
+
+arr = []
+buff = input()
+while buff != "":
+    arr.append(int(buff))
+    buff = input()
+
+sum = int(input("Escreva a soma:"))
+
+for i in arr:
+    for j in arr:
+        if i + j == sum:
+            print(f"{i}, {j}")
