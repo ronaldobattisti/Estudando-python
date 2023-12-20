48934782355659e721aaf492a9d77ef964b5d99d
ronaldobattisti <ronaldo.battisti@hotmail.com>
2023-12-19 21:58:09 -0300

    Join of all exercices

---
+++ Tribonacci.py
@@ -0,0 +1,12 @@
+size = int(input("Escreva o tamanho da lista Tribonacci: "))
+
+tribonacci = [0, 0, 1]
+
+i = 2
+
+while i < size:
+    buffer = tribonacci[i - 2] + tribonacci[i - 1] + tribonacci[i]
+    tribonacci.append(buffer)
+    i += 1
+
+print(tribonacci)
