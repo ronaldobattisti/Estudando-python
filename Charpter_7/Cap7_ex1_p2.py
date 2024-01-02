33040219f7b143541052c8ccbb967d48bd1178e0
SAZI | Ronaldo Battisti <rbattisti@sazi.com.br>
2024-01-02 17:08:21 -0300

    charp 7 exercices

---
+++ Charpter_7/Cap7_ex1_p2.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    try:
+        for i in range(16):
+            a = float(input(f"Write the {i+1}° number: "))
+        
+
+        
+    except ValueError as e:
+        print(f"Error: {e}")
