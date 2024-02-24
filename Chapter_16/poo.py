6b1287cc8f6c2142ff143c06b8d8b6e1cf6f3f18
ronaldobattisti <ronaldo.battisti@hotmail.com>
2024-02-23 22:52:47 -0300

    Charpt 16 1st class

---
+++ Chapter_16/poo.py
@@ -0,0 +1,22 @@
+"""
+poo is a programming paradigm used to map real life object
+
+Main ellements of poo
+- Class -> model of the real world ellement being represented computationally
+- Attribute -> object's characteristics
+- Method -> object behavior (functions)
+- Builder -> Special method used to create objects
+- Object -> Class's instance
+"""
+number = 10
+print(number)
+print(type(number))
+
+
+class Product:
+    pass
+
+ps4 = Product()
+
+print(ps4)
+print(type(ps4))
